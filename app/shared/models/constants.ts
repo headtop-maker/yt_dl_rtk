3f8d10eecb87e8f4546cac25baaf250649460867
vadiuninda <vadiuninda@lad24.ru>
2025-09-01 08:35:04 +0300

    debug:  выгрузка андроид

--- app/shared/models/constants.ts
+++ app/shared/models/constants.ts
@@ -1,3 +1,3 @@
-export const DEV_API = 'https://ytdlapptest.duckdns.org';
-
+// export const DEV_API = 'https://ytdlapptest.duckdns.org';
+export const DEV_API = 'http://192.168.77.10:3000';
 export const dlFetchApiDomain = 'dlFetchApiDomain';
